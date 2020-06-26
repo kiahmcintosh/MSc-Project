ae43f0f3bc34eb7aae6efe136e3448d5a9600ffa
kiahmcintosh <gitkiah@stinkycheese.co.uk>
2020-06-26 14:03:49 +0100

    fixed modified cosine

--- similarity_calc.py
+++ similarity_calc.py
@@ -3,6 +3,7 @@ import numpy
 import sys
 
 class Peak:
+    """Class to hold an MS2 peak"""
     def __init__(self,mass,intensity):
         self.mass = mass
         self.intensity = intensity
@@ -10,13 +11,7 @@ class Peak:
     
     def __repr__(self):
         return f"{self.__class__.__name__}({self.mass},{self.intensity})"
-
-    def compare_peaks(self, peak_2, tolerance):
-        """Compares to another peak and returns true if masses are within given tolerance"""
-        if (self.mass >= peak_2.mass-tolerance and self.mass <= peak_2.mass+tolerance):
-            return True
-        else:
-            return False
+        
 
 class Spectrum:
     def __init__(self):
@@ -32,12 +27,6 @@ class Spectrum:
     def set_peak_id(self,feature_id):
         self.feature_id = feature_id
 
-    def compare_precursor_mass(self,spectrum_2,threshold):
-        if ((self.pep_mass-spectrum_2.pep_mass)>(-threshold) and (self.pep_mass-spectrum_2.pep_mass)<threshold):
-            return True
-        else:
-            return False
-    
     def euclidean_scale(self):
         """Scales the intensity of each Peak object using Euclidean norm"""
         
@@ -54,7 +43,6 @@ class Spectrum:
             peak.norm_scaled=(peak.sqrt_intensity)/norm
 
 
-
 def mgf_reader(file_path):
     """Takes the path to a .mgf file. Returns a list of Spectrum objects, each containing the attributes contained in the
     file and a list of Peak objects"""
@@ -65,6 +53,7 @@ def mgf_reader(file_path):
     #initiate list to hold all the Spectrum objects
     spectra_list=[]
 
+    # for line in lines[:1000]:  ##alternative to test subset of data
     for line in lines:
         if 'BEGIN IONS' in line:
             spectrum_lines = [] #initiate new spectra list to contain lines of the next spectra data
@@ -92,6 +81,8 @@ def mgf_reader(file_path):
                         spectrum.charge = value
                     elif "mslevel".casefold() in parameter.casefold():
                         spectrum.ms_level = int(value)
+                    elif "name".casefol() in parameter.casefold():
+                        spectrum.name=value
 
                 #when it reaches a line with no '=' then it is one of the peaks, which is added to the spectrum
                 else:
@@ -112,18 +103,23 @@ def mgf_reader(file_path):
     return spectra_list
 
 
-def cosine_score(spectrum_one, spectrum_two, tolerance):
-    """takes two MS2 Spectrum objects and returns the cosine similarity score"""  
+def cosine_score(spectrum_one, spectrum_two, tolerance, modification=0):
+    """takes two MS2 Spectrum objects, with the fragment tolerance and returns the cosine similarity score.
+    If modification mass is given then modified cosine is returned"""
     peak_pairs=[]
+
     #make list of pairs of matched peaks within given tolerance
     for peak1 in spectrum_one.peaks:
         for peak2 in spectrum_two.peaks:
-            if peak1.compare_peaks(peak2,tolerance):
+
+            #check normal and modified peak masses
+            if (abs(peak1.mass-peak2.mass)<=tolerance) or (abs(peak1.mass+modification-peak2.mass) <= tolerance):
                 product=(peak1.norm_scaled)*(peak2.norm_scaled)
-                #pair_list is a list of tuples saying which two peaks are matched and the product of their intensities
                 tuple = (peak1, peak2, product)
+                #add to list of peak pairs
                 peak_pairs.append(tuple)
-    
+            
+
     #sort matching peaks by descending intensity product
     peak_pairs.sort(reverse=True,key=lambda tuple: tuple[2])
     
@@ -148,31 +144,62 @@ def cosine_score(spectrum_one, spectrum_two, tolerance):
             #add to the list of used peaks
             used_peaks_one.append(peak_one)
             used_peaks_two.append(peak_two)
-       
+
     return total
+
+
+def filter_pairs(pairs):
+    #sort by cosine
+    pairs.sort(reverse=True,key=lambda spectra_pair: spectra_pair[2])
+
+    """rest of funtion to be added"""
+
 
 def main(file_path):
     """Give mgf file path as argument.
     Returns a list of spectrum pairs, showing the two spectrum IDs and the cosine similarity score of each pair"""
-
 
     #make list of spectrum objects from mgf file
     spectra_list=mgf_reader(file_path)
 
     spectra_matches=[]
 
+    precursor_tolerance=1.0 #haven't used this, need to work out where to use
+    fragment_tolerance=0.3
+    modification_tolerance=400
+
     for spectrum_one in spectra_list:
         for spectrum_two in spectra_list:
-            if spectrum_one.compare_precursor_mass(spectrum_two,1):#check if precursor masses are close enough to calculate cosine similarity
+            modification=spectrum_two.pep_mass-spectrum_one.pep_mass
+
+            #check if precursor masses are close enough to calculate cosine similarity
+            if modification<=modification_tolerance and modification >= -modification_tolerance:
                 
-                #get spectrum IDs and calculate similarity
-                spectrum_match=(spectrum_one.feature_id,spectrum_two.feature_id,cosine_score(spectrum_one,spectrum_two,0.3))
+                #make tuple of spectra IDs and modified cosine score
+                spectrum_match=(spectrum_one.feature_id,spectrum_two.feature_id,cosine_score(
+                    spectrum_one,spectrum_two,fragment_tolerance,modification))
+                
                 spectra_matches.append(spectrum_match)
     
     return spectra_matches
 
-if __name__ == "__main__":
-    main(sys.argv[1])
+##to use from command line by giving path to mgf file as argument
+#if __name__ == "__main__":
+#    main(sys.argv[1])
+
+
+
+##test time taken and number of pairs found
+# import time
+# start = time.ctime()
+# pairs=main(".\example_data\MS2_peaks.mgf")
+# end = time.ctime()
+# print(pairs)
+# print(f"start: {start}\tend: {end}")
+# print(len(pairs))
+##example file with 993 spectra, 986049 possible pairs, 962697 matched in ~4 mins
+
+
 
 
 
